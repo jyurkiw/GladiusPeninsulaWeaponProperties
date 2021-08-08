a0b235e9818e83059c48e83e1a71102c8ca65895
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2021-08-08 03:19:14 -0700

    Update gladiuspeninsulawp.js

--- gladiuspeninsulawp.js
+++ gladiuspeninsulawp.js
@@ -3,7 +3,6 @@ Hooks.on('ready', () => {
   CONFIG.DND5E.weaponProperties['multipleshots'] = 'Multiple Shots';
   CONFIG.DND5E.weaponProperties['multiplebarrels'] = 'Multiple Barrels';
   CONFIG.DND5E.weaponProperties['brace'] = 'Brace';
-  CONFIG.DND5E.weaponProperties['multiplebarrels'] = 'Multiple Barrels';
   CONFIG.DND5E.weaponProperties['Missfires'] = 'Missfires';
   CONFIG.DND5E.weaponProperties['malfunctions'] = 'Malfunctions';
   CONFIG.DND5E.weaponProperties['fogofwar'] = 'Fog of War';
