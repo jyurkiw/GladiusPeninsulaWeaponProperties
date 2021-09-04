63bf5f0150ed0dc444a0bc006506f04c4e1f0b7a
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2021-09-04 00:46:39 -0700

    Added Lucky Shot weapon property.

--- gladiuspeninsulawp.js
+++ gladiuspeninsulawp.js
@@ -3,7 +3,8 @@ Hooks.on('ready', () => {
   CONFIG.DND5E.weaponProperties['multipleshots'] = 'Multiple Shots';
   CONFIG.DND5E.weaponProperties['multiplebarrels'] = 'Multiple Barrels';
   CONFIG.DND5E.weaponProperties['brace'] = 'Brace';
-  CONFIG.DND5E.weaponProperties['Missfires'] = 'Missfires';
+  CONFIG.DND5E.weaponProperties['missfires'] = 'Missfires';
   CONFIG.DND5E.weaponProperties['malfunctions'] = 'Malfunctions';
+  CONFIG.DND5E.weaponProperties['lucky_shot'] = 'Lucky Shot';
   CONFIG.DND5E.weaponProperties['fogofwar'] = 'Fog of War';
 });
